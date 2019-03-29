be6b37a9fb99e8a41509b195d574755a65076998
John Morgan <john.morgan@icrar.org>
2019-03-29 23:52:18 +0800

    remove tabs!

--- make_imstack2.py
+++ make_imstack2.py
@@ -100,90 +100,90 @@ df.attrs['USER'] = os.environ['USER']
 df.attrs['DATE_CREATED'] = datetime.datetime.utcnow().isoformat()
 
 for band in opts.bands:
-	if band is None:
-		group = df['/']
-	elif not band in df.keys():
-		group = df.create_group(band)
-	else:
-		logging.warn("Warning, overwriting existing band %s", band)
-		group = df[band]
-	group.attrs['TIME_INTERVAL'] = opts.step
+    if band is None:
+        group = df['/']
+    elif not band in df.keys():
+        group = df.create_group(band)
+    else:
+        logging.warn("Warning, overwriting existing band %s", band)
+        group = df[band]
+    group.attrs['TIME_INTERVAL'] = opts.step
 
-	# determine data size and structure 
-	if band is None:
-		image_file = FILENAME.format(obsid=obsid, time=opts.start, pol=opts.pols[0], suffix=opts.suffixes[0])
-	else:
-		image_file = FILENAME_BAND.format(obsid=obsid, band=band, time=opts.start, pol=opts.pols[0], suffix=opts.suffixes[0])
-	hdus = fits.open(image_file, memmap=True)
-	image_size = hdus[HDU].data.shape[-1]
-	assert image_size % opts.stamp_size== 0, "image_size must be divisible by stamp_size"
-	data_shape = [len(opts.pols), image_size, image_size, N_CHANNELS, opts.n]
-	chunks = (len(opts.pols), opts.stamp_size, opts.stamp_size, N_CHANNELS, opts.n)
+    # determine data size and structure 
+    if band is None:
+        image_file = FILENAME.format(obsid=obsid, time=opts.start, pol=opts.pols[0], suffix=opts.suffixes[0])
+    else:
+        image_file = FILENAME_BAND.format(obsid=obsid, band=band, time=opts.start, pol=opts.pols[0], suffix=opts.suffixes[0])
+    hdus = fits.open(image_file, memmap=True)
+    image_size = hdus[HDU].data.shape[-1]
+    assert image_size % opts.stamp_size== 0, "image_size must be divisible by stamp_size"
+    data_shape = [len(opts.pols), image_size, image_size, N_CHANNELS, opts.n]
+    chunks = (len(opts.pols), opts.stamp_size, opts.stamp_size, N_CHANNELS, opts.n)
 
-	beam_shape = data_shape[:-1] + [1] # just one beam for all timesteps for now
-	beam = group.create_dataset("beam", beam_shape, dtype=np.float32, compression='gzip', shuffle=True)
-	for p, pol in enumerate(opts.pols):
-		if band is None:
-			hdus = fits.open(PB_FILE.format(obsid=obsid, pol=pol), memmap=True)
-		else:
-			hdus = fits.open(PB_FILE_BAND.format(obsid=obsid, band=band, pol=pol), memmap=True)
-		beam[p, :, :, 0, 0] = hdus[HDU].data[SLICE]
-		for key, item in hdus[0].header.iteritems():
-			beam.attrs[key] = item
-	pb_sum = np.sqrt(np.sum(beam[...]**2, axis=0)/len(opts.pols))
-	pb_mask = pb_sum > opts.pb_thresh*np.nanmax(pb_sum)
-	pb_nan = pb_sum/pb_sum
-	if np.any(np.isnan(pb_sum)):
-		logging.warn("NaNs in primary beam")
+    beam_shape = data_shape[:-1] + [1] # just one beam for all timesteps for now
+    beam = group.create_dataset("beam", beam_shape, dtype=np.float32, compression='gzip', shuffle=True)
+    for p, pol in enumerate(opts.pols):
+        if band is None:
+            hdus = fits.open(PB_FILE.format(obsid=obsid, pol=pol), memmap=True)
+        else:
+            hdus = fits.open(PB_FILE_BAND.format(obsid=obsid, band=band, pol=pol), memmap=True)
+        beam[p, :, :, 0, 0] = hdus[HDU].data[SLICE]
+        for key, item in hdus[0].header.iteritems():
+            beam.attrs[key] = item
+    pb_sum = np.sqrt(np.sum(beam[...]**2, axis=0)/len(opts.pols))
+    pb_mask = pb_sum > opts.pb_thresh*np.nanmax(pb_sum)
+    pb_nan = pb_sum/pb_sum
+    if np.any(np.isnan(pb_sum)):
+        logging.warn("NaNs in primary beam")
 
-	# write main header information
-	timesteps = group.create_dataset("WSCTIMES", (opts.n,), dtype=np.uint16)
-	timesteps2 = group.create_dataset("WSCTIMEE", (opts.n,), dtype=np.uint16)
-	if band is None:
-		header_file = FILENAME.format(obsid=obsid, time=opts.n//2, pol=opts.pols[0], suffix=opts.suffixes[0])
-	else:
-		header_file = FILENAME_BAND.format(obsid=obsid, band=band, time=opts.n//2, pol=opts.pols[0], suffix=opts.suffixes[0])
-	# add fits header to attributes
-	hdus = fits.open(header_file, memmap=True)
-	header = group.create_dataset('header', data=[], dtype=DTYPE)
-	for key, item in hdus[0].header.iteritems():
-		header.attrs[key] = item
+    # write main header information
+    timesteps = group.create_dataset("WSCTIMES", (opts.n,), dtype=np.uint16)
+    timesteps2 = group.create_dataset("WSCTIMEE", (opts.n,), dtype=np.uint16)
+    if band is None:
+        header_file = FILENAME.format(obsid=obsid, time=opts.n//2, pol=opts.pols[0], suffix=opts.suffixes[0])
+    else:
+        header_file = FILENAME_BAND.format(obsid=obsid, band=band, time=opts.n//2, pol=opts.pols[0], suffix=opts.suffixes[0])
+    # add fits header to attributes
+    hdus = fits.open(header_file, memmap=True)
+    header = group.create_dataset('header', data=[], dtype=DTYPE)
+    for key, item in hdus[0].header.iteritems():
+        header.attrs[key] = item
 
-	for s, suffix in enumerate(opts.suffixes):
-		# gzip is rather slower than lzf, but is more standard in hdf5. Will allow dumping to h5dump etc.
-		data = group.create_dataset(suffix, data_shape, chunks=chunks, dtype=DTYPE, compression='gzip', shuffle=True)
-		filenames = group.create_dataset("%s_filenames" % suffix, (len(opts.pols), N_CHANNELS, opts.n), dtype="S%d" % len(header_file), compression='gzip')
-	
-		n_rows = image_size/opts.n_pass
-		for i in range(opts.n_pass):
-			logging.info("processing segment %d/%d" % (i+1, opts.n_pass))
-			for t in xrange(opts.n):
-				im_slice = [slice(n_rows*i, n_rows*(i+1)), slice(None, None, None)]
-				fits_slice = SLICE[:-2] + im_slice
+    for s, suffix in enumerate(opts.suffixes):
+        # gzip is rather slower than lzf, but is more standard in hdf5. Will allow dumping to h5dump etc.
+        data = group.create_dataset(suffix, data_shape, chunks=chunks, dtype=DTYPE, compression='gzip', shuffle=True)
+        filenames = group.create_dataset("%s_filenames" % suffix, (len(opts.pols), N_CHANNELS, opts.n), dtype="S%d" % len(header_file), compression='gzip')
+    
+        n_rows = image_size/opts.n_pass
+        for i in range(opts.n_pass):
+            logging.info("processing segment %d/%d" % (i+1, opts.n_pass))
+            for t in xrange(opts.n):
+                im_slice = [slice(n_rows*i, n_rows*(i+1)), slice(None, None, None)]
+                fits_slice = SLICE[:-2] + im_slice
 
-				for p, pol in enumerate(opts.pols):
+                for p, pol in enumerate(opts.pols):
 
-					if band is None:
-						infile = FILENAME.format(obsid=obsid, time=t+opts.start, pol=pol, suffix=suffix)
-					else:
-						infile = FILENAME_BAND.format(obsid=obsid, band=band, time=t+opts.start, pol=pol, suffix=suffix)
-					logging.info(" processing %s", infile)
-					hdus = fits.open(infile, memmap=True)
-					filenames[p, 0, t] = infile
-					print data[p, n_rows*i:n_rows*(i+1), :, 0, t].shape
-					print hdus[0].data[fits_slice].shape
-					print pb_mask.shape
-					data[p, n_rows*i:n_rows*(i+1), :, 0, t] = np.where(pb_mask[n_rows*i:n_rows*(i+1), :, 0, 0],
-																	   hdus[0].data[fits_slice],
-																	   np.nan)*pb_nan[n_rows*i:n_rows*(i+1), :, 0, 0]
-					if s==0 and p==0:
-						timesteps[t] = hdus[0].header['WSCTIMES']
-						timesteps2[t] = hdus[0].header['WSCTIMEE']
-					else:
-						# NB these are *not* enforced across different frequency bands, but these could, in principle, have different TIME_INTERVALS
-						if not opts.skip_check_wcs_timesteps:
-							assert timesteps[t] == hdus[0].header['WSCTIMES'], "Timesteps do not match %s in %s" % (opts.suffixes[0], infile)
-							assert timesteps2[t] == hdus[0].header['WSCTIMEE'], "Timesteps do not match %s in %s" % (opts.suffixes[0], infile)
-						else:
-							logging.debug(hdus[0].header['DATE-OBS'])
-							logging.debug(hdus[0].header['DATE-OBS'])
+                    if band is None:
+                        infile = FILENAME.format(obsid=obsid, time=t+opts.start, pol=pol, suffix=suffix)
+                    else:
+                        infile = FILENAME_BAND.format(obsid=obsid, band=band, time=t+opts.start, pol=pol, suffix=suffix)
+                    logging.info(" processing %s", infile)
+                    hdus = fits.open(infile, memmap=True)
+                    filenames[p, 0, t] = infile
+                    print data[p, n_rows*i:n_rows*(i+1), :, 0, t].shape
+                    print hdus[0].data[fits_slice].shape
+                    print pb_mask.shape
+                    data[p, n_rows*i:n_rows*(i+1), :, 0, t] = np.where(pb_mask[n_rows*i:n_rows*(i+1), :, 0, 0],
+                                                                       hdus[0].data[fits_slice],
+                                                                       np.nan)*pb_nan[n_rows*i:n_rows*(i+1), :, 0, 0]
+                    if s==0 and p==0:
+                        timesteps[t] = hdus[0].header['WSCTIMES']
+                        timesteps2[t] = hdus[0].header['WSCTIMEE']
+                    else:
+                        # NB these are *not* enforced across different frequency bands, but these could, in principle, have different TIME_INTERVALS
+                        if not opts.skip_check_wcs_timesteps:
+                            assert timesteps[t] == hdus[0].header['WSCTIMES'], "Timesteps do not match %s in %s" % (opts.suffixes[0], infile)
+                            assert timesteps2[t] == hdus[0].header['WSCTIMEE'], "Timesteps do not match %s in %s" % (opts.suffixes[0], infile)
+                        else:
+                            logging.debug(hdus[0].header['DATE-OBS'])
+                            logging.debug(hdus[0].header['DATE-OBS'])
